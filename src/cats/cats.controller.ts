d459e4920511ca452fdbde96bf3053e153805518
HuongNguyenQuoc <quochuong05ln@gmail.com>
2026-07-26 10:36:33 +0700

    Init nestjs

---
+++ src/cats/cats.controller.ts
@@ -0,0 +1,19 @@
+import { Body, Controller, Post, Get } from '@nestjs/common';
+import { CatsService } from 'src/cats/cats.service';
+import { CreateCatDto } from './dto/create-cat.dto';
+import { Cat } from './interfaces/cat.interface';
+
+@Controller('cats')
+export class CatsController {
+  constructor(private catsService: CatsService) {}
+
+  @Post()
+  async create(@Body() createCatDto: CreateCatDto) {
+    this.catsService.create(createCatDto);
+  }
+
+  @Get()
+  async findAll(): Promise<Cat[]> {
+    return this.catsService.findAll();
+  }
+}
